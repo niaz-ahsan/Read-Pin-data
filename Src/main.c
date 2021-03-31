8500792ac3a04f75ca8364a6eaa0d9b02e034f1e
niaz-ahsan <thatisniaz@gmail.com>
2021-03-30 23:01:00 -0700

    LD turn off method added and operation is taking place inside a super loop

--- Src/main.c
+++ Src/main.c
@@ -5,17 +5,13 @@
  */
 
 #include <stdint.h>
-#include <stdio.h>
 
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-void turn_on_LED(uint32_t *const RCC_AHB1) {
-	uint32_t *const GPIO_D_mode = (uint32_t*) 0x40020C00; // GPIO D Mode Register Address
-	uint32_t *const GPIO_D_output_data = (uint32_t*) 0x40020C14; // GPIO D O/P Data Register Address
-
+void turn_on_LED(uint32_t *const RCC_AHB1, uint32_t *const GPIO_D_mode, uint32_t *const GPIO_D_output_data) {
 	// setting 3rd bit of RCC Clock register to enable GPIO D peripheral
 	*RCC_AHB1 |= (1 << 3);
 
@@ -29,8 +25,15 @@ void turn_on_LED(uint32_t *const RCC_AHB1) {
 	*GPIO_D_output_data |= (1 << 12);
 }
 
+void turn_off_LED(uint32_t *const GPIO_D_output_data) {
+	// clearing 12 bit position of GPIO D o/p data, controlled by PD12
+	*GPIO_D_output_data &= ~(1 << 12);
+}
+
 int main(void)
 {
+	uint32_t *const GPIO_D_mode = (uint32_t*) 0x40020C00; // GPIO D Mode Register Address
+	uint32_t *const GPIO_D_output_data = (uint32_t*) 0x40020C14; // GPIO D O/P Data Register Address
 
 	// enable RCC AHB1 peripheral reset register for GPIO A
 	uint32_t *const RCC_AHB1 = (uint32_t*) 0x40023830;
@@ -42,19 +45,19 @@ int main(void)
 	*GPIOA_port_mode_reg &= ~(3 << 0);
 
 	// GPIO A Peripheral i/p data register address for PA0
-    uint32_t *const GPIOA_input_data_reg = (uint32_t*) 0x40020010;
+	uint32_t *const GPIOA_input_data_reg = (uint32_t*) 0x40020010;
 
-    uint8_t pin_status = (uint8_t) *GPIOA_input_data_reg & 0x01;
+	while(1) {
+		uint8_t pin_status = (uint8_t) *GPIOA_input_data_reg & 0x01;
 
-    if(pin_status) {
-    	// turn on LED
-    	turn_on_LED(RCC_AHB1);
-    } else {
-    	// turn off LED
-    }
+		if(pin_status) {
+			// turn on LED
+			turn_on_LED(RCC_AHB1, GPIO_D_mode, GPIO_D_output_data);
+		} else {
+			// turn off LED
+			turn_off_LED(GPIO_D_output_data);
+		}
+	}
 
-    printf("Result is: %d\n", pin_status);
 
-
-	while(1);
 }
