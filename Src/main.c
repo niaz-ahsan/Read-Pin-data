18011b6ef1e3a2a83d58d1ab60788c5967de7872
niaz-ahsan <thatisniaz@gmail.com>
2021-04-03 11:59:35 -0700

    volatile added instead of const to be optimize compatible

--- Src/main.c
+++ Src/main.c
@@ -11,7 +11,7 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-void turn_on_LED(uint32_t *const RCC_AHB1, uint32_t *const GPIO_D_mode, uint32_t *const GPIO_D_output_data) {
+void turn_on_LED(uint32_t volatile *RCC_AHB1, uint32_t volatile *GPIO_D_mode, uint32_t volatile *GPIO_D_output_data) {
 	// setting 3rd bit of RCC Clock register to enable GPIO D peripheral
 	*RCC_AHB1 |= (1 << 3);
 
@@ -25,27 +25,27 @@ void turn_on_LED(uint32_t *const RCC_AHB1, uint32_t *const GPIO_D_mode, uint32_t
 	*GPIO_D_output_data |= (1 << 12);
 }
 
-void turn_off_LED(uint32_t *const GPIO_D_output_data) {
+void turn_off_LED(uint32_t volatile *GPIO_D_output_data) {
 	// clearing 12 bit position of GPIO D o/p data, controlled by PD12
 	*GPIO_D_output_data &= ~(1 << 12);
 }
 
 int main(void)
 {
-	uint32_t *const GPIO_D_mode = (uint32_t*) 0x40020C00; // GPIO D Mode Register Address
-	uint32_t *const GPIO_D_output_data = (uint32_t*) 0x40020C14; // GPIO D O/P Data Register Address
+	uint32_t volatile *GPIO_D_mode = (uint32_t*) 0x40020C00; // GPIO D Mode Register Address
+	uint32_t volatile *GPIO_D_output_data = (uint32_t*) 0x40020C14; // GPIO D O/P Data Register Address
 
 	// enable RCC AHB1 peripheral reset register for GPIO A
-	uint32_t *const RCC_AHB1 = (uint32_t*) 0x40023830;
+	uint32_t volatile *RCC_AHB1 = (uint32_t*) 0x40023830;
 	*RCC_AHB1 |= (1 << 0); // enabling 0th bit for GPIO A
 
 	// GPIO A port mode register address
-	uint32_t *const GPIOA_port_mode_reg = (uint32_t*) 0x40020000;
+	uint32_t volatile *GPIOA_port_mode_reg = (uint32_t*) 0x40020000;
 	// 0,1 bit position should be cleared for setting it into input mode
 	*GPIOA_port_mode_reg &= ~(3 << 0);
 
 	// GPIO A Peripheral i/p data register address for PA0
-	uint32_t *const GPIOA_input_data_reg = (uint32_t*) 0x40020010;
+	uint32_t volatile *GPIOA_input_data_reg = (uint32_t*) 0x40020010;
 
 	while(1) {
 		uint8_t pin_status = (uint8_t) *GPIOA_input_data_reg & 0x01;
